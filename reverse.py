cec61877c4f9c88b22306256577eabde0b4b6f23
Nikita-1432011 <steppers.nikita3159@gmail.com>
2025-07-03 20:47:57 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,9 @@
+string = input("Enter your own string:")
+
+string2 = ('')
+
+for i in string:
+    string2 = i + string2
+
+print("\nThe original word(string)=",string)
+print("The reversed word(string)=",string2)
